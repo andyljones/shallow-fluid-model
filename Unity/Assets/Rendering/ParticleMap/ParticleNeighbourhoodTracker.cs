d81bd1fa44e05cd8c16584164f51c30a75307db7
Andrew Jones <andyjones.ed@gmail.com>
2014-02-14 16:06:24 +0000

    Optimized out 5/6ths of all calls to KDTree.

--- Unity/Assets/Rendering/ParticleMap/ParticleNeighbourhoodTracker.cs
+++ Unity/Assets/Rendering/ParticleMap/ParticleNeighbourhoodTracker.cs
@@ -12,11 +12,32 @@ namespace Assets.Rendering.ParticleMap
     public class ParticleNeighbourhoodTracker
     {
         private readonly KDTree _vertexTree;
+        private readonly Vector3[] _vertices;
+        private readonly int[][] _neighbourIndices;
+        private readonly Vector3[][] _neighbours;
 
-        public ParticleNeighbourhoodTracker(IPolyhedron polyhedron)
+        private int[] _indicesOfNearestVertex;
+
+        public ParticleNeighbourhoodTracker(IPolyhedron polyhedron, int particleCount)
         {
-            var vertices = GetVertexPositions(polyhedron);
-            _vertexTree = KDTree.MakeFromPoints(vertices);
+            _vertices = GetVertexPositions(polyhedron);
+
+            _vertexTree = KDTree.MakeFromPoints(_vertices);
+            _neighbourIndices = VertexIndexedTableFactory.Neighbours(polyhedron);
+            _neighbours = BuildVertexNeighbourTable(_neighbourIndices, _vertices);
+
+            _indicesOfNearestVertex = new int[particleCount];
+        }
+
+        private static Vector3[][] BuildVertexNeighbourTable(int[][] neighbourIndices, Vector3[] vertices)
+        {
+            var neighbours = new Vector3[vertices.Length][];
+            for (int i = 0; i < vertices.Length; i++)
+            {
+                neighbours[i] = neighbourIndices[i].Select(j => vertices[j]).ToArray();
+            }
+
+            return neighbours;
         }
 
         private static Vector3[] GetVertexPositions(IPolyhedron surface)
@@ -24,9 +45,47 @@ namespace Assets.Rendering.ParticleMap
             return surface.Vertices.Select(vertex => GraphicsUtilities.Vector3(vertex.Position)).ToArray();
         }
 
-        public int[] GetIndicesOfVerticesNearest(Vector3 position)
+        public int[] GetIndicesOfVerticesNearest(Vector3[] particlePositions)
         {
-            return new[] {_vertexTree.FindNearest(position)};
+            for (int i = 0; i < particlePositions.Length; i++)
+            {
+                _indicesOfNearestVertex[i] = GetIndexOfNearest(i, particlePositions[i]);
+            }
+
+            return _indicesOfNearestVertex;
+        }
+
+        private int GetIndexOfNearest(int particleIndex, Vector3 particlePosition)
+        {
+            var indexOfPreviousNearestVertex = _indicesOfNearestVertex[particleIndex];
+
+            if (CheckIfAnyNeighbourIsCloser(particlePosition, indexOfPreviousNearestVertex))
+            {
+                return _vertexTree.FindNearest(particlePosition);
+            }
+            else
+            {
+                return indexOfPreviousNearestVertex;
+            }
+        }
+
+        private bool CheckIfAnyNeighbourIsCloser(Vector3 particlePosition, int indexOfPreviousNearestVertex)
+        {
+            var previousNearestVertex = _vertices[indexOfPreviousNearestVertex];
+            var previousNeighbours = _neighbours[indexOfPreviousNearestVertex];
+
+            var sqrDistanceToNearest = (particlePosition - previousNearestVertex).sqrMagnitude;
+            for (int j = 0; j < previousNeighbours.Length; j++)
+            {
+                var sqrDistanceToNeighbour = (particlePosition - previousNeighbours[j]).sqrMagnitude;
+
+                if (sqrDistanceToNeighbour < sqrDistanceToNearest)
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
 
     }
--- Unity/Assets/Rendering/ParticleMap/ParticlePositionUpdater.cs
+++ Unity/Assets/Rendering/ParticleMap/ParticlePositionUpdater.cs
@@ -13,21 +13,30 @@ namespace Assets.Rendering.ParticleMap
         private readonly ParticleNeighbourhoodTracker _tracker;
 
         private readonly IParticleMapOptions _options;
+        private readonly Vector3[] _vertexPositions;
 
         public ParticlePositionUpdater(IPolyhedron polyhedron, IParticleMapOptions options)
         {
             _options = options;
 
-            _tracker = new ParticleNeighbourhoodTracker(polyhedron);
+            _tracker = new ParticleNeighbourhoodTracker(polyhedron, options.ParticleCount);
+            _vertexPositions = GetVertexPositions(polyhedron);
+        }
+
+        private static Vector3[] GetVertexPositions(IPolyhedron surface)
+        {
+            return surface.Vertices.Select(vertex => GraphicsUtilities.Vector3(vertex.Position)).ToArray();
         }
 
         public void Update(ref Vector3[] particlePositions, VectorField<Vertex> velocityField)
         {
+            var indicesOfNearestVertices = _tracker.GetIndicesOfVerticesNearest(particlePositions);
+
             var newParticlePositions = new Vector3[particlePositions.Count()];
             for (int i = 0; i < newParticlePositions.Count(); i++)
             {
                 var position = particlePositions[i];
-                var velocity = GetVelocity(velocityField, position);
+                var velocity = GetVelocity(velocityField, indicesOfNearestVertices[i]);
                 var newPosition = CalculateNewPosition(velocity, position);
                 newParticlePositions[i] = newPosition;
             }
@@ -48,12 +57,28 @@ namespace Assets.Rendering.ParticleMap
             return newPosition;
         }
 
-        private Vector3 GetVelocity(VectorField<Vertex> velocityField, Vector3 particlePosition)
+        private Vector3 GetVelocity(VectorField<Vertex> velocityField, int nearestVertex)
         {
-            var indices = _tracker.GetIndicesOfVerticesNearest(particlePosition);
-            var velocity = velocityField[indices.First()];
+            //var p1 = _vertexPositions[indices[0]];
+            //var p2 = _vertexPositions[indices[1]];
+            //var p3 = _vertexPositions[indices[2]];
 
-            return GraphicsUtilities.Vector3(_options.WindmapScaleFactor * _options.Timestep * velocity);
+            //var f1 = p1 - f;
+            //var f2 = p2 - f;
+            //var f3 = p3 - f;
+
+            //var a = Vector3.Cross(p1 - p2, p1 - p3).magnitude;
+
+            //var a1 = Vector3.Cross(f2, f3).magnitude / a;
+            //var a2 = Vector3.Cross(f3, f1).magnitude / a;
+            //var a3 = Vector3.Cross(f1, f2).magnitude / a;
+
+            var uv1 = velocityField[nearestVertex];
+            //var uv2 = velocityField[indices[1]];
+            //var uv3 = velocityField[indices[2]];
+            //var uv = uv1 * a1 + uv2 * a2 + uv3 * a3;
+
+            return GraphicsUtilities.Vector3(_options.WindmapScaleFactor * _options.Timestep * uv1);
         }
     }
 }
