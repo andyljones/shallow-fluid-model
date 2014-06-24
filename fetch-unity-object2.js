18d466762d7adc8b3cc7356b10a91cafbb870b6d
Andy Jones <andyjones.ed@gmail.com>
2014-06-24 13:42:52 +0100

    Added files for the climate model page

---
+++ fetch-unity-object2.js
@@ -0,0 +1,4 @@
+var unityObjectUrl = "http://webplayer.unity3d.com/download_webplayer-3.x/3.0/uo/UnityObject2.js";
+if (document.location.protocol == 'https:')
+	unityObjectUrl = unityObjectUrl.replace("http://", "https://ssl-");
+document.write('<script type="text\/javascript" src="' + unityObjectUrl + '"><\/script>');
